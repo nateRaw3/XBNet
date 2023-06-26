d38ef3e53706f3a8d8971af8196d95fb02208cb5
nateRaw3 <108367771+nateRaw3@users.noreply.github.com>
2023-06-26 15:34:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,13 +28,13 @@ setuptools.setup(
 
 
 		 install_requires=[
-		"sklearn==0.0",
-		"numpy == 1.22.0",
-		"pandas == 1.3.3",
-		"matplotlib==3.4.3",
-		 "torch==1.9.0",
-        "xgboost==1.4.2",
-		"tqdm==4.62.2"
+		"sklearn>=0.0",
+		"numpy >= 1.22.0",
+		"pandas >= 1.3.3",
+		"matplotlib>=3.4.3",
+		 "torch>=1.9.0",
+        "xgboost>=1.4.2",
+		"tqdm>=4.62.2"
 
 	],
 
